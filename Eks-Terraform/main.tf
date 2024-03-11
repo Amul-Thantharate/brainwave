3a7c45a28bcf9e46aa1fc0d5d43fe0bd797254d9
Amul-Thantharate <amulthantharate@gmail.com>
2024-03-11 18:23:55 +0530

    Update EKS Terraform configuration

--- Eks-Terraform/main.tf
+++ Eks-Terraform/main.tf
@@ -1,47 +1,57 @@
 data "aws_iam_policy_document" "assume_role" {
     statement {
-        effect = "Allow"
-        principals {
-            type        = "Service"
-            identifiers = ["eks.amazon.com"]
-        }
-        actions = ["sts:AssumeRole"] 
+    effect = "Allow"
+
+    principals {
+        type        = "Service"
+        identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
     }
 }
 
-resource "aws_iam_role" "eks-brainwave" {
-    name               = "eks-brainwave"
+resource "aws_iam_role" "example" {
+    name               = "eks-cluster-cloud"
     assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "brainwave-AmazonEKSClusterPolicy" {
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
     policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-    role = aws_iam_role.eks-brainwave.name
+    role       = aws_iam_role.example.name
 }
 
+#get vpc data
 data "aws_vpc" "default" {
     default = true
 }
-
+#get public subnets for cluster
 data "aws_subnets" "public" {
     filter {
-        name = "vpc-id"
+        name   = "vpc-id"
         values = [data.aws_vpc.default.id]
     }
 }
+#cluster provision
+resource "aws_eks_cluster" "example" {
+    name     = "EKS_CLOUD"
+    role_arn = aws_iam_role.example.arn
 
-resource "aws_eks_cluster" "brainwave" {
-    name = "brainwave"
-    role_arn = aws_iam_role.eks-brainwave.arn
     vpc_config {
         subnet_ids = data.aws_subnets.public.ids
     }
-    depends_on = [ aws_iam_role_policy_attachment.brainwave-AmazonEKSClusterPolicy, ]
+
+    # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+    # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+    depends_on = [
+        aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+    ]
 }
 
-resource "aws_iam_role" "brainwave1" {
-    name = "brainwave1"
-        assume_role_policy = jsonencode({
+resource "aws_iam_role" "example1" {
+    name = "eks-node-group-cloud"
+
+    assume_role_policy = jsonencode({
         Statement = [{
         Action = "sts:AssumeRole"
         Effect = "Allow"
@@ -53,40 +63,40 @@ resource "aws_iam_role" "brainwave1" {
     })
 }
 
-resource "aws_iam_instance_profile" "brainwave1" {
-    name = "brainwave1"
-    role = aws_iam_role.brainwave1.name
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+    role       = aws_iam_role.example1.name
 }
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
     policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-    role = aws_iam_role.brainwave1.name
+    role       = aws_iam_role.example1.name
 }
 
-resource "aws_iam_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
-    name = "example-AmazonEKSWorkerNodePolicy"
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-    roles = [aws_iam_role.brainwave1.name]
-}
 resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
     policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-    role       = aws_iam_role.brainwave1.name
+    role       = aws_iam_role.example1.name
 }
 
-resource "aws_eks_node_group" "brainwave" {
-    cluster_name = "brainwave"
-    node_group_name = "brainwave-ng"
-    node_role_arn = aws_iam_role.brainwave1.arn
-    subnet_ids = data.aws_subnets.public.ids
+#create node group
+resource "aws_eks_node_group" "example" {
+    cluster_name    = aws_eks_cluster.example.name
+    node_group_name = "brainwave-node"
+    node_role_arn   = aws_iam_role.example1.arn
+    subnet_ids      = data.aws_subnets.public.ids
+
     scaling_config {
-        desired_size = 2
+        desired_size = 1
         max_size     = 2
         min_size     = 1
     }
-    instance_types = [ "t2.medium" ]
-    depends_on = [ 
-        aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+    instance_types = ["t2.medium"]
+
+    # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+    # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+    depends_on = [
         aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+        aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
         aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
-    ]
+        ]
 }
